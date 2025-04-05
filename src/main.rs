5e6a86843ac37f7f44b1305fadccfea00f172cdc
D-K-E <D-K-E@users.noreply.github.com>
2025-04-06 01:32:43 +0900

    concurrent img gradient works, now refactor it

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 //
-use rusty_ray::imgio::imrender::save_pixels;
-use rusty_ray::imgio::imtypes::Pixel;
-use rusty_ray::imgio::imtypes::Point2d;
-use rusty_ray::selfsync::pipeline::add_to_pipeline;
-use smol::channel::Sender;
-use smol::channel::unbounded;
-use smol::future::race;
-use std::thread::scope;
+use rusty_ray::{
+    imgio::{
+        imrender::save_pixels,
+        imtypes::{Pixel, Point2d},
+    },
+    selfsync::pipeline::add_to_pipeline,
+};
+use smol::{
+    Executor,
+    channel::{Receiver, Sender, unbounded},
+    future::block_on,
+};
 // use std::future::{Future, FutureExt};
 
 struct ImGradientData {
@@ -30,15 +34,15 @@ fn imgrad2pix(imgrad: ImGradientData) -> Pixel {
     let x = imgrad.x;
     let y = imgrad.y;
     let img_width = imgrad.img_width;
-    let img_height = imgrad.img_height;
-    let red_ = (x as f32) / (img_width as f32);
-    let green_ = (y as f32) / (img_height as f32);
+    let img_height: u32 = imgrad.img_height;
+    let red_: f32 = (x as f32) / (img_width as f32);
+    let green_: f32 = (y as f32) / (img_height as f32);
     let blue_: f32 = 0.25;
     let red = red_ * 255.9;
     let green = green_ * 255.9;
-    let blue = blue_ * 255.9;
-    let data = image::Rgb([red as u8, green as u8, blue as u8]);
-    let coord = Point2d { x: x, y: y };
+    let blue: f32 = blue_ * 255.9;
+    let data: image::Rgb<u8> = image::Rgb([red as u8, green as u8, blue as u8]);
+    let coord: Point2d = Point2d { x: x, y: y };
     Pixel::new(data, coord)
 }
 
@@ -54,45 +58,80 @@ fn generate_img(img_width: u32, img_height: u32) -> Vec<Pixel> {
     result
 }
 
-fn process_image_gradient(imw: u32, imh: u32, input_send: Sender<ImGradientData>) -> () {
-    for x in 0..imw {
-        for y in 0..imh {
-            let imgrad = ImGradientData::new(x, y, imw, imh);
-            input_send.send(imgrad).await;
-        }
-    }
-    drop(input_send);
+fn gen_imgraddata<'tasklife>(
+    imw: u32,
+    imh: u32,
+    quit: &'tasklife Receiver<bool>,
+    ex: &mut Executor<'tasklife>,
+) -> (usize, Receiver<ImGradientData>) {
+    let tw = imw.clone();
+    let th = imh.clone();
+    let nb_tasks: usize = (imw * imh) as usize;
+    let (input_send, input_receive) = unbounded::<ImGradientData>();
+    let _t = ex
+        .spawn(async move {
+            for x in 0..imw {
+                for y in 0..imh {
+                    match quit.try_recv() {
+                        Ok(_) => break,
+                        _ => (),
+                    }
+                    let imgrad = ImGradientData::new(x, y, tw, th);
+                    let _ = input_send.send(imgrad).await;
+                }
+            }
+            drop(input_send);
+            println!("input_send closed");
+        })
+        .detach();
+    println!("finished sending");
+    return (nb_tasks, input_receive);
 }
 
-fn generate_img_concurrent(img_width: &u32, img_height: &u32) -> Vec<Pixel> {
-    // input channel
-    let (input_send, input_receive) = unbounded::<ImGradientData>();
-    let (quit_send, quit_receive) = unbounded::<bool>();
-    println!("created quit and input channels");
-    let pixel_receive = add_to_pipeline(&quit_receive, imgrad2pix, &input_receive);
-    println!("created pixel receive");
-
-    scope(|scope| {
-        scope.spawn(move |_| {
-            process_image_gradient(img_width, img_height, input_send);
-            // .boxed()
-        });
-        // notice that we are not returning the child scope join handler
-        // by adding the semicolumn
-    })
-    .unwrap();
-    println!("sent all data");
-
-    // collect from output channel to result
+async fn collect_output<'tasklife>(
+    out_r: &'tasklife Receiver<Pixel>,
+    quit: &'tasklife Sender<bool>,
+    nb_tasks: &'tasklife usize,
+) -> Vec<Pixel> {
     let mut result = Vec::<Pixel>::new();
-    loop {
-        match pixel_receive.recv() {
+    while result.len() < (*nb_tasks) {
+        match out_r.recv().await {
             Ok(pix) => {
                 result.push(pix);
             }
-            Err(_) => break,
+            _ => (),
         }
     }
+    println!(
+        "result filled: result len {}, nb_tasks {}",
+        result.len(),
+        *nb_tasks
+    );
+    out_r.close();
+    drop(out_r);
+    quit.send(true).await;
+    drop(quit);
+    result
+}
+
+fn generate_img_concurrent(img_width: u32, img_height: u32) -> Vec<Pixel> {
+    // input channel
+    let (q_s, q_r) = unbounded::<bool>();
+    let mut ex = Executor::new();
+    let (nb_tasks, input_receive) = gen_imgraddata(img_width, img_height, &q_r, &mut ex);
+    let bind = input_receive.clone();
+    let out_receiver = add_to_pipeline(&q_r, imgrad2pix, bind, &mut ex);
+    //
+    let result = block_on(ex.run(async { collect_output(&out_receiver, &q_s, &nb_tasks).await }));
+    while !ex.is_empty() {
+        ex.try_tick();
+    }
+    // input_send.close();
+    // drop(input_send);
+    out_receiver.close();
+    input_receive.close();
+    q_s.close();
+    q_r.close();
     result
 }
 
@@ -112,7 +151,7 @@ fn sequential_main() {
 fn concurrent_main() {
     let im_width: u32 = 256;
     let im_height: u32 = 256;
-    let pixels = generate_img_concurrent(&im_width, &im_height);
+    let pixels = generate_img_concurrent(im_width, im_height);
     save_pixels(
         im_width,
         im_height,
--- src/selfsync/pipeline.rs
+++ src/selfsync/pipeline.rs
@@ -1,43 +1,57 @@
 //! basic task pipelinening
+use smol::Executor;
 use smol::channel::Receiver;
 use smol::channel::Sender;
 use smol::channel::unbounded;
-use smol::{Executor};
 use std::marker;
 
 async fn apply_send_task<Input: marker::Send, Output: marker::Send>(
-    task: &fn(Input) -> Output,
+    task: fn(Input) -> Output,
     input: &Receiver<Input>,
     out: &Sender<Output>,
 ) {
-    match input.try_recv() {
-        Ok(task_input) => {
-            let output = task(task_input);
-            out.send(output).await;
-        }
-        Err(_) => {}
-    }
 }
 
-pub fn add_to_pipeline<'tasklife, Input: marker::Send, Output: marker::Send + 'tasklife>(
+pub fn add_to_pipeline<
+    'tasklife,
+    Input: marker::Send + 'tasklife,
+    Output: marker::Send + 'tasklife,
+>(
     quit: &'tasklife Receiver<bool>,
     task: fn(Input) -> Output,
-    input: &'tasklife Receiver<Input>,
+    input: Receiver<Input>,
     ex: &mut Executor<'tasklife>,
 ) -> Receiver<Output> {
     let (out_sender, out_receiver) = unbounded::<Output>();
 
-    let t = ex.spawn(async move {
-        loop {
-            match quit.try_recv() {
-                Ok(_) => {
-                    break;
+    let t = ex
+        .spawn(async move {
+            println!("starting pipeline loop");
+            loop {
+                match quit.try_recv() {
+                    Ok(msg) => {
+                        break;
+                    }
+                    _ => (),
                 }
-                Err(_) => apply_send_task(&task, input, &out_sender).await,
+                match input.recv().await {
+                    Ok(task_input) => {
+                        let output = task(task_input);
+                        let _ = out_sender.send(output).await;
+                    }
+                    Err(err) => {
+                        if input.is_closed() {
+                            println!("input closed");
+                            break;
+                        }
+                    }
+                }
             }
-        }
-        drop(out_sender);
-    });
+            out_sender.close();
+            println!("out sender closed");
+            drop(out_sender);
+        })
+        .detach();
     // notice that we are not returning the child scope join handler
     // by adding the semicolumn
     return out_receiver;
